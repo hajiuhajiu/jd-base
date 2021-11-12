43322b417b7a799d3ae946352fea55988865aaee
hajiuhajiu <2705322732@qq.com>
2021-11-12 20:40:05 +0800

    Update gitpull1.sh

--- gitpull1.sh
+++ gitpull1.sh
@@ -60,7 +60,7 @@ function Git_PullShell {
 ## 克隆scripts
 function Git_CloneScripts {
   echo -e "克隆脚本\n"
-  git clone -b main ${ScriptsURL} ${ScriptsDir}
+  git clone -b master ${ScriptsURL} ${ScriptsDir}
   ExitStatusScripts=$?
   echo
 }
