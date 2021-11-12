a4a248ed7a1dc002f7d2f424baa2a2119825935f
hajiuhajiu <2705322732@qq.com>
2021-11-12 19:45:34 +0800

    Update git_pull.sh

--- git_pull.sh
+++ git_pull.sh
@@ -23,7 +23,7 @@ ContentNewTask=${ShellDir}/new_task
 ContentDropTask=${ShellDir}/drop_task
 SendCount=${ShellDir}/send_count
 isTermux=${ANDROID_RUNTIME_ROOT}${ANDROID_ROOT}
-ShellURL=https://github.com/hajiuhajiu/jd-shell
+ShellURL=https://github.com/hajiuhajiu/jd-base
 ScriptsURL=https://github.com/hajiuhajiu/scripts
 
 ## 更新crontab，gitee服务器同一时间限制5个链接，因此每个人更新代码必须错开时间，每次执行git_pull随机生成。
