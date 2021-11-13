131a32453fc06ce8fc5d9f8d4955d36d7fa8cb1f
hajiuhajiu <2705322732@qq.com>
2021-11-13 08:04:48 +0800

    Update del.sh

--- del.sh
+++ del.sh
@@ -1,29 +1,28 @@
-rm /root/jd/log -r
-rm /root/jd/scripts/jd_hyj_jinbi.js
-rm /root/jd/scripts/jd_temp_hyj.js
-rm /root/jd/scripts/jd_NewSign.js
-rm /root/jd/scripts/jd_decompression.js 
-rm /root/jd/scripts/main.01a74c39.js
-rm /root/jd/scripts/app.2c58486f.js
-rm /root/jd/scripts/jd_jxg.js
-rm /root/jd/scripts/app.4864fa34.js
-rm /root/jd/scripts/app.53f47a0c.js
-rm /root/jd/scripts/app.eb41fc5f.js
-rm /root/jd/scripts/jd_dwnc.py
-if [ ! -f "/root/jd/scripts/jd_fcwb.js" ];
+rm /data/data/com.termux/files/home/jd-base/log -r
+rm /data/data/com.termux/files/home/jd-base/scripts/jd_hyj_jinbi.js
+rm /data/data/com.termux/files/home/jd-base/scripts/jd_temp_hyj.js
+rm /data/data/com.termux/files/home/jd-base/scripts/jd_NewSign.js
+rm /data/data/com.termux/files/home/jd-base/scripts/jd_decompression.js 
+rm /data/data/com.termux/files/home/jd-base/scripts/main.01a74c39.js
+rm /data/data/com.termux/files/home/jd-base/scripts/app.2c58486f.js
+rm /data/data/com.termux/files/home/jd-base/scripts/jd_jxg.js
+rm /data/data/com.termux/files/home/jd-base/scripts/app.4864fa34.js
+rm /data/data/com.termux/files/home/jd-base/scripts/app.53f47a0c.js
+rm /data/data/com.termux/files/home/jd-base/scripts/app.eb41fc5f.js
+rm /data/data/com.termux/files/home/jd-base/scripts/jd_dwnc.py
+if [ ! -f "/data/data/com.termux/files/home/jd-base/scripts/jd_fcwb.js" ];
 then  echo "文件不存在"
-else  rm -f /root/jd/scripts/jd_fcwb.js
+else  rm -f /data/data/com.termux/files/home/jd-base/scripts/jd_fcwb.js
 fi
-if [ ! -x " /root/jd/.github/workflows" ]; 
+if [ ! -x " /data/data/com.termux/files/home/jd-base/.github/workflows" ]; 
 then echo "文件夹不存在" 
-else rm /root/jd/.github/workflows -r
+else rm /data/data/com.termux/files/home/jd-base/.github/workflows -r
 fi
-if [ ! -f "/root/jd/scripts/jd_lol.js" ];
+if [ ! -f "/data/data/com.termux/files/home/jd-base/scripts/jd_lol.js" ];
 then  echo "文件不存在"
-else  rm -f /root/jd/scripts/jd_lol.js
+else  rm -f /data/data/com.termux/files/home/jd-base/scripts/jd_lol.js
 fi
-if [ ! -f "/root/jd/scripts/jd_djyyj.js" ];
+if [ ! -f "/data/data/com.termux/files/home/jd-base/scripts/jd_djyyj.js" ];
 then  echo "文件不存在"
-else  rm -f /root/jd/scripts/jd_djyyj.js
+else  rm -f /data/data/com.termux/files/home/jd-base/scripts/jd_djyyj.js
 fi
-
