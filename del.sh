e7bce0e1523cd4a184b4031f360e31d7a817f84e
hajiuhajiu <2705322732@qq.com>
2021-11-16 09:01:37 +0800

    Update del.sh

--- del.sh
+++ del.sh
@@ -1,28 +1,52 @@
-rm /data/data/com.termux/files/home/jd-base/log -r
-rm /data/data/com.termux/files/home/jd-base/scripts/jd_hyj_jinbi.js
-rm /data/data/com.termux/files/home/jd-base/scripts/jd_temp_hyj.js
-rm /data/data/com.termux/files/home/jd-base/scripts/jd_NewSign.js
-rm /data/data/com.termux/files/home/jd-base/scripts/jd_decompression.js 
-rm /data/data/com.termux/files/home/jd-base/scripts/main.01a74c39.js
-rm /data/data/com.termux/files/home/jd-base/scripts/app.2c58486f.js
-rm /data/data/com.termux/files/home/jd-base/scripts/jd_jxg.js
-rm /data/data/com.termux/files/home/jd-base/scripts/app.4864fa34.js
-rm /data/data/com.termux/files/home/jd-base/scripts/app.53f47a0c.js
-rm /data/data/com.termux/files/home/jd-base/scripts/app.eb41fc5f.js
-rm /data/data/com.termux/files/home/jd-base/scripts/jd_dwnc.py
-if [ ! -f "/data/data/com.termux/files/home/jd-base/scripts/jd_fcwb.js" ];
+rm /root/jd/log -r
+
+rm /root/jd/scripts/jd_dwnc.py
+
+if [ ! -f "/root/jd/scripts/jd_fcwb.js" ];
 then  echo "文件不存在"
-else  rm -f /data/data/com.termux/files/home/jd-base/scripts/jd_fcwb.js
+else  rm -f /root/jd/scripts/jd_fcwb.js
 fi
-if [ ! -x " /data/data/com.termux/files/home/jd-base/.github/workflows" ]; 
+if [ ! -x " /root/jd/.github/workflows" ]; 
 then echo "文件夹不存在" 
-else rm /data/data/com.termux/files/home/jd-base/.github/workflows -r
+else rm /root/jd/.github/workflows -r
 fi
-if [ ! -f "/data/data/com.termux/files/home/jd-base/scripts/jd_lol.js" ];
+if [ ! -f "/root/jd/scripts/jd_lol.js" ];
 then  echo "文件不存在"
-else  rm -f /data/data/com.termux/files/home/jd-base/scripts/jd_lol.js
+else  rm -f /root/jd/scripts/jd_lol.js
 fi
-if [ ! -f "/data/data/com.termux/files/home/jd-base/scripts/jd_djyyj.js" ];
+if [ ! -f "/root/jd/scripts/jd_djyyj.js" ];
 then  echo "文件不存在"
-else  rm -f /data/data/com.termux/files/home/jd-base/scripts/jd_djyyj.js
+else  rm -f /root/jd/scripts/jd_djyyj.js
 fi
+if [ ! -f "/root/jd/scripts/JD_extra_cookie.js" ];
+then  echo ""
+else  rm -f /root/jd/scripts/JD_extra_cookie.js 
+fi
+if [ ! -f "/root/jd/scripts/jd_GoldcoinToGift.js" ];
+then  echo ""
+else  rm -f /root/jd/scripts/jd_GoldcoinToGift.js 
+fi
+if [ ! -f "/root/jd/scripts/jd_ShopSign.js" ];
+then  echo ""
+else  rm -f /root/jd/scripts/jd_ShopSign.js
+fi
+if [ ! -f "/root/jd/scripts/jd_carnivalcity.js" ];
+then  echo ""
+else  rm -f /root/jd/scripts/jd_carnivalcity.js
+fi
+if [ ! -f "/root/jd/scripts/jd_chinajoy.js" ];
+then  echo ""
+else  rm -f /root/jd/scripts/jd_chinajoy.js
+fi
+
+if [ ! -f "/root/jd/scripts/jd_cfdtx.js" ];
+then  echo ""
+else  rm -f /root/jd/scripts/jd_cfdtx.js
+fi
+ShellDir=${JD_DIR:-$(cd $(dirname $0); pwd)}
+ScriptsDir=${ShellDir}/scripts
+if [ ! -f "ScriptsDir/jd_rankingList.js" ];
+then  echo "目录文件不存在"
+else  rm -f ScriptsDir/jd_rankingList.js
+fi
+
